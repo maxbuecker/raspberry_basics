196977c017b21630d2381d211dd619d61afab582
maxbuecker <76119731+maxbuecker@users.noreply.github.com>
2021-07-30 22:35:31 +0200

    Update LEDs.py

--- LEDs.py
+++ LEDs.py
@@ -10,16 +10,16 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(green, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(red, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
 
-n = 10
+n = 0
+nmax = 30
 
-for i in range(n):
-  if (i==0) & (i==2) & (i==4) & (i==6) & (i==8):
-    GPIO.output(green,GPIO.HIGH) #turn on green LED
-    GPIO.output(red, GPIO.LOW) #turn off red LED
-  else:
-    GPIO.output(green,GPIO.LOW) #turn off green LED
-    GPIO.output(red, GPIO.HIGH) #turn on red LED
-  print('Iteration ' + str(i) + '/' + str(n))
-  time.sleep(1000)
-
+while n != nmax:
+  GPIO.output(green,GPIO.HIGH) #turn on green LED
+  GPIO.output(red, GPIO.LOW) #turn off red LED
+  time.sleep(500)
+  GPIO.output(green,GPIO.LOW) #turn off green LED
+  GPIO.output(red, GPIO.HIGH) #turn on red LED
+  time.sleep(500)
+  n += 1
+  
 GPIO.cleanup()
