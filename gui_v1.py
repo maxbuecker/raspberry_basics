a6a82b7ec8e8fc1e82de05aeb656f973615a4db1
maxbuecker <76119731+maxbuecker@users.noreply.github.com>
2021-07-31 13:44:46 +0200

    Update gui_v1.py

--- gui_v1.py
+++ gui_v1.py
@@ -1,45 +1,72 @@
 #!/usr/bin/python3
 
 #import libraries for GUI
+
+import datetime
+from demo_stop_watch import *
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QDialog, QApplication
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
 
-#define GUI
-class GUI(QWidget):
-
+class MyForm(QDialog):
     def __init__(self):
         super().__init__()
-        self.title = 'WEATHER CHANNEL'
-        self.width = 400
-        self.height = 300
-        self.left = 100
-        self.top = 100
-        self.initUI()
-    
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        
-        start-button = QPushButton('Start Recording', self)
-        start-button.setToolTip('Start Button')
-        start-button.move(150,100)
-        start-button.clicked.connect(self.on_click)
-        
-        stop-button = QPushButton('Stop Recording', self)
-        stop-button.setToolTip('Stop Button')
-        stop-button.move(150,200)
-        stop-button.clicked.connect(self.on_click)
-        
-        self.show()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.run_watch)
+        self.timer.setInterval(1)
+        self.mscounter = 0
+        self.isreset = True
+# creating slots for event handling (button press)
+        self.ui.pushButtonStart.clicked.connect(self.start_watch)
+        self.ui.pushButtonStop.clicked.connect(self.stop_watch)
+        self.ui.pushButtonPause.clicked.connect(self.watch_pause)
+        self.ui.pushButtonReset.clicked.connect(self.watch_reset)
+        self.showLCD()
 
-    @pyqtSlot()
-    def on_click(self):
-        print('Recording started')
-        START = True
-        
+def showLCD(self):
+    text = str(datetime.timedelta(milliseconds=self.mscounter))[:-3]
+    self.ui.lcdNumber.setDigitCount(11)
+    if not self.isreset:  # if "isreset" is False
+        self.ui.lcdNumber.display(text)
+    else:
+        self.ui.lcdNumber.display('0:00:00.000')
+def run_watch(self):
+    self.mscounter += 1
+    self.showLCD()
+def start_watch(self):
+    self.timer.start()
+    self.isreset = False
+    self.ui.pushButtonReset.setDisabled(True)
+    self.ui.pushButtonStart.setDisabled(True)
+    self.ui.pushButtonStop.setDisabled(False)
+    self.ui.pushButtonPause.setDisabled(False)
+def stop_watch(self):
+    self.timer.stop()
+    self.mscounter = 0
+    self.ui.pushButtonReset.setDisabled(False)
+    self.ui.pushButtonStart.setDisabled(False)
+    self.ui.pushButtonStop.setDisabled(True)
+    self.ui.pushButtonPause.setDisabled(True)
+def watch_pause(self):
+    self.timer.stop()
+    self.ui.pushButtonReset.setDisabled(False)
+    self.ui.pushButtonStart.setDisabled(False)
+    self.ui.pushButtonStop.setDisabled(True)
+    self.ui.pushButtonPause.setDisabled(True)
+def watch_reset(self):
+    self.timer.stop()
+    self.mscounter = 0
+    self.isreset = True
+    self.showLCD()
+    self.ui.pushButtonReset.setDisabled(True)
+    self.ui.pushButtonStart.setDisabled(False)
+    self.ui.pushButtonStop.setDisabled(True)
+    self.ui.pushButtonPause.setDisabled(True)
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    gui = GUI()
+    w = MyForm()
+    w.show()
     sys.exit(app.exec_())
